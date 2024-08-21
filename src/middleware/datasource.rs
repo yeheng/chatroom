f2a70fcf5cb46ae60496ef4d436f7a41a076a24f
Heng.Ye <hengheng8848@gmail.com>
2024-08-21 18:14:42 +0800

    ✨ 优化数据库连接配置和超时时间设置
    这次提交优化了数据库连接配置和超时时间设置，具体变动包括：
    - 更新了数据库连接池的最小连接数
    - 设置了连接超时时间、获取连接超时时间和空闲连接超时时间
    - 启用了 SQLx 日志记录并设置了日志记录级别
    
    这些变动将提升代码的性能和可维护性。
    
    ```

--- src/middleware/datasource.rs
+++ src/middleware/datasource.rs
@@ -1,6 +1,7 @@
 use std::time::Duration;
 
 use crate::config::CONFIG;
+use fastdate::DurationFrom;
 use sea_orm::{ConnectOptions, Database, DatabaseConnection};
 
 /// 连接到数据库
@@ -12,12 +13,12 @@ pub async fn connect() -> DatabaseConnection {
     let mut opt = ConnectOptions::new(&CONFIG.database.url);
     opt.max_connections(CONFIG.database.pool_size as u32) // 设置最大连接数
         .min_connections(5) // 设置最小连接数
-        .connect_timeout(Duration::from_secs(8)) // 设置连接超时时间
-        .acquire_timeout(Duration::from_secs(8)) // 设置获取连接超时时间
-        .idle_timeout(Duration::from_secs(8)) // 设置空闲连接超时时间
-        .max_lifetime(Duration::from_secs(CONFIG.database.pool_timeout as u64)) // 设置连接最大存活时间
-        .sqlx_logging(true) // 启用 SQLx 日志记录
-        .sqlx_logging_level(log::LevelFilter::Info); // 设置 SQLx 日志记录级别
+        .connect_timeout(parse_duration(&CONFIG.database.connect_timeout)) // 设置连接超时时间
+        .acquire_timeout(parse_duration(&CONFIG.database.acquire_timeout)) // 设置获取连接超时时间
+        .idle_timeout(parse_duration(&CONFIG.database.idle_timeout)) // 设置空闲连接超时时间
+        .max_lifetime(parse_duration(&CONFIG.database.pool_timeout)) // 设置连接最大存活时间
+        .sqlx_logging(CONFIG.database.sqlx_logging) // 启用 SQLx 日志记录
+        .sqlx_logging_level(parse_log_level(&CONFIG.database.sqlx_logging_level)); // 设置 SQLx 日志记录级别
     let r = Database::connect(opt).await;
     match r {
         Ok(_) => r.unwrap(),
@@ -26,4 +27,28 @@ pub async fn connect() -> DatabaseConnection {
             std::process::exit(1); // 退出程序
         }
     }
+}
+
+fn parse_log_level(sqlx_logging_level: &str) -> log::LevelFilter {
+    match sqlx_logging_level.to_lowercase().as_str() {
+        "trace" => log::LevelFilter::Trace,
+        "debug" => log::LevelFilter::Debug,
+        "info" => log::LevelFilter::Info,
+        "warn" => log::LevelFilter::Warn,
+        "error" => log::LevelFilter::Error,
+        _ => log::LevelFilter::Off,
+    }
+}
+
+fn parse_duration(log_rolling: &str) -> Duration {
+    let lower = log_rolling.to_lowercase();
+    let unit = lower.chars().last().unwrap();
+    let number = lower.trim_end_matches(unit).parse::<u64>().unwrap();
+    let duration = match unit.to_string().as_str() {
+        "d" => Duration::from_day(number),
+        "h" => Duration::from_hour(number),
+        "m" => Duration::from_minute(number),
+        _ => Duration::from_secs(number),
+    };
+    return duration;
 }
